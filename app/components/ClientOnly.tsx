97bc1ca910eee8dc96dd48281d02bceecab30599
alimmondal <italimbd@gmail.com>
2023-10-25 07:00:17 +0600

    first commit

---
+++ app/components/ClientOnly.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { useEffect, useState } from "react";
+
+interface ClientOnlyProps {
+  children: React.ReactNode;
+}
+
+const ClientOnly: React.FC<ClientOnlyProps> = ({ children }) => {
+  const [hasMount, setHasMount] = useState(false);
+  useEffect(() => {
+    setHasMount(true);
+  }, []);
+  if (!hasMount) {
+    return null;
+  }
+
+  return <>{children}</>;
+};
+
+export default ClientOnly;
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="text-rose-500 text-2xl">
+      <h1>Air Bnb clone</h1>
+    </main>
+  );
+}
